6f85f62d84b9811f0d6f87ec914cd66d5213d381
Constantine Krotov <ckwebmast@gmail.com>
2022-08-09 03:43:15 +0300

    comleted

---
+++ script.js
@@ -0,0 +1,36 @@
+const guessNumBot = () => { function res () {
+  let randomNum = parseInt(Math.random() * 100)
+  let userNum = prompt('Угадай число от 1 до 100')
+  console.log(userNum)
+
+
+  if (isNaN(userNum)) {
+     confirm('Введи число!')
+  }
+
+  if (userNum === null) {
+    alert('Игра окончена')
+  }
+
+  if (userNum === randomNum) {
+    alert('Поздравляю, Вы угадали!!!')
+  
+  }  else if (userNum > randomNum) {
+    prompt('Загаданное число меньше')
+
+  } else if (userNum < randomNum && userNum !== null) {
+    prompt('Загаданное число больше')
+
+  } 
+
+
+
+}
+  
+  
+  return res
+}
+
+let guessBot = guessNumBot()
+
+guessBot()
